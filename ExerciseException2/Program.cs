09c69e355f315d367c1fe482523e9f58f2feaed6
Lucas Ortis <lucasortis01@gmail.com>
2021-09-18 14:14:02 -0300

    Implementation of Account class and DomainException Exceptions

--- ExerciseException2/Program.cs
+++ ExerciseException2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using ExerciseException2.Entities;
+using ExerciseException2.Entities.Exceptions;
 
 namespace ExerciseException2
 {
@@ -6,7 +9,34 @@ namespace ExerciseException2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Enter account data: ");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double withdrawValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                acc.Withdraw(withdrawValue);
+
+                Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            
+
         }
     }
 }
